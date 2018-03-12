8538f6cc1bc7fefb153e5435159e45c8ebbdf0dd
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2018-03-12 03:59:55 +0100

    Fix break while Class python ...

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from network import Network
 from utils import Utils
 
 class run():
-    ut = Utils()
-    network = Network(ut)
-    network.attackTarget()
+    def __init__(self, ut):
+        network = Network(ut)
+        network.attackTarget()
 
 if __name__ == '__main__':
+    ut = Utils()
     while 1:
-       main = run()
+       main = run(ut)
